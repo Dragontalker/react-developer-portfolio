947bca7574d17318a0ef4aec1878e18c1318e541
Richard <richard.yang.tong@gmail.com>
2021-03-24 11:33:11 -0400

    Added a Home link.

--- src/components/Narbar.js
+++ src/components/Narbar.js
@@ -2,6 +2,9 @@ const Navbar = () => {
     return (
         <nav>
             <h1>The Dojo Blog</h1>
+            <div>
+                <a href="/">Home</a>
+            </div>
         </nav>
     )
 };
