d3334f6bd5fb9a59df01bb850b37bd7c7756d9f3
Richard <richard.yang.tong@gmail.com>
2021-03-24 18:36:45 -0400

    Resolved image import issue.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
+import backgroundImage from "../Assets/Image/Background-website-01.jpg";
+
 const Home = () => {
     return (
         <div>
             <div id="homeTitle">
                 <img 
-                    src="./Assets/Image/Background-website-01.jpg" 
+                    src= {backgroundImage}
                     alt="A background image of random connected vectors." 
                     className="img-fluid"
                 />
