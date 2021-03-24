578059f6f187adec68d46fbe4c00d69dbfe2a97b
Richard <richard.yang.tong@gmail.com>
2021-03-24 18:42:40 -0400

    Imported styled sheet.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import './App.css';
 
 // Import components
 import { Project, Contact } from './components/Pages';
