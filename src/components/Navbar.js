30800b05b0aef7750f53546d0ec30f9163435ee8
Richard <richard.yang.tong@gmail.com>
2021-03-24 16:11:35 -0400

    Added a navbar that appears on every page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,26 @@
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is Navbar</h1>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav mx-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#homeTitle">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#aboutTitle">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#portfolioTitle">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#contactTitle">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 };
 
