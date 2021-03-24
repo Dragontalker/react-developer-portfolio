5e2827d1d898657ec50649b34a42f820d144739b
Richard <richard.yang.tong@gmail.com>
2021-03-24 16:14:58 -0400

    Filled in the Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
 const Home = () => {
-
+    return (
+        <div>
+            <div id="homeTitle">
+                <img 
+                    src="./Assets/Image/Background-website-01.jpg" 
+                    alt="A background image of random connected vectors." 
+                    className="img-fluid"
+                />
+            </div>
+            <div className="heading-content text-center">
+                <h5>Hello, I'm</h5>
+                <h1>Richard Yang</h1>
+                <p>A Full-stack Developer</p>
+            </div>
+        </div>
+    )
 };
 
 export default Home;
