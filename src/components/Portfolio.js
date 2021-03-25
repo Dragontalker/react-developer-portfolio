cb4fe9413ea5e46054e2d52a92abb2caae824222
Richard <richard.yang.tong@gmail.com>
2021-03-24 20:29:00 -0400

    Added the first project.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,3 +1,7 @@
+import Project from './Project';
+import p1Image from '../Assets/Image/burger_screenshot.png';
+import p2Image from '../Assets/Image/flux_im.png';
+
 const Portfolio = () => {
     return (
         <div>
@@ -8,7 +12,16 @@ const Portfolio = () => {
                 <h3 class="display-4 font-weight-bold">Portfolio</h3>
             </div>
             <div id="portfolio" class="container mt-5 pb-5">
-                <div class="row"></div>
+                <div class="row">
+                    <Project 
+                        img={p1Image}
+                        title={"Express Burger Handler"}
+                        description={"A full stack project that ultilizes the concept of MVC design pattern and rendering power of express handlebars."}
+                        deployment={"https://dragontalker-eat-da-burger.herokuapp.com/"}
+                        repo={"https://github.com/Dragontalker/FullStack_BurgerHandler"}
+                    />
+                    
+                </div>
             </div>
         </div>
     )
--- src/components/Project.js
+++ src/components/Project.js
@@ -10,8 +10,8 @@ const Project = (prop) => {
                 <div class="card-body">
                     <h5 class="card-title">{prop.title}</h5>
                     <p class="card-text">{prop.description}</p>
-                    <a href={prop.deployment} class="btn btn-dark">Live Preview</a>
-                    <a href={prop.repo} class="btn btn-dark">GitHub Repo</a>
+                    <a href={prop.deployment} class="btn btn-dark m-1">Live Preview</a>
+                    <a href={prop.repo} class="btn btn-dark m-1">GitHub Repo</a>
                 </div>
             </div>
         </div>
